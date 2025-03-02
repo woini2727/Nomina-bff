27d09a6c0bdeefebea1ea0de6db07986c1562982
Salvador <salvador.woini@gmail.com>
2025-03-02 00:56:30 -0300

    chore: initial commit

---
+++ src/test/java/com/bff/nomina/nomina/NominaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bff.nomina.nomina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
