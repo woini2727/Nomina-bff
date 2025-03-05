a1f26dddf6bebd51b6a7e288236e6860fb72910c
Salvador <salvador.woini@gmail.com>
2025-03-05 10:07:50 -0300

    feat: add usecase

--- src/main/java/com/bff/nomina/nomina/application/usecase/SearchNominaUseCase.java
+++ src/main/java/com/bff/nomina/nomina/application/usecase/SearchNominaUseCase.java
@@ -1,17 +1,36 @@
 package com.bff.nomina.nomina.application.usecase;
 
 import com.bff.nomina.nomina.application.port.in.SearchNominaQuery;
+import com.bff.nomina.nomina.application.port.out.NominaRepository;
+import com.bff.nomina.nomina.domain.Nomina;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executor;
 
 @Component
 @Slf4j
 public class SearchNominaUseCase implements SearchNominaQuery {
+    private final NominaRepository nominaRepository;
+    private final Executor executor;
+
+    public SearchNominaUseCase(NominaRepository nominaRepository,
+                               final @Qualifier("asyncExecutor") Executor executor) {
+        this.nominaRepository = nominaRepository;
+        this.executor = executor;
+    }
 
     @Override
-    public CompletionStage<String> getByCuit(String cuit) {
-        return null;
+    public CompletionStage<Nomina> getByCuit(String cuit) {
+        return CompletableFuture.supplyAsync(() -> doGet(cuit), executor);
     }
+
+    private Nomina doGet(String cuit) {
+        return nominaRepository.searchByCuit(cuit);
+    }
+
+
 }
