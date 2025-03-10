e26a7a5c835bab3de1dc2c52a2d148716249efbc
Salvador <salvador.woini@gmail.com>
2025-03-10 10:51:06 -0300

    feat: update project config

--- src/main/java/com/bff/nomina/nomina/config/AppConfig.java
+++ src/main/java/com/bff/nomina/nomina/config/AppConfig.java
@@ -6,14 +6,15 @@ import org.springframework.cloud.sleuth.instrument.async.LazyTraceExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.Executor;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
-
+    private static final int CORE_POOL_SIZE = 20;
+    private static final int MAX_POOL_SIZE = 1000;
+    private static final String ASYNC_PREFIX = "async-";
     private static final boolean WAIT_FOR_TASK_TO_COMPLETE_ON_SHUTDOWN = true;
 
     private final BeanFactory beanFactory;
@@ -25,14 +26,12 @@ public class AppConfig implements WebMvcConfigurer {
     }
 
     @Bean("asyncExecutor")
-    public Executor getAsyncExecutor(final AsyncConfig asyncConfig) {
-        final AsyncConfig.ExecutorConfig defaultExecutorConfig = asyncConfig.getDefaultExecutor();
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(defaultExecutorConfig.getCorePoolSize());
-        executor.setMaxPoolSize(defaultExecutorConfig.getMaxPoolSize());
-        executor.setQueueCapacity(defaultExecutorConfig.getQueueCapacity());
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
         executor.setWaitForTasksToCompleteOnShutdown(WAIT_FOR_TASK_TO_COMPLETE_ON_SHUTDOWN);
-        executor.setThreadNamePrefix(defaultExecutorConfig.getThreadNamePrefix());
+        executor.setThreadNamePrefix(ASYNC_PREFIX);
         executor.initialize();
 
         return new LazyTraceExecutor(beanFactory, executor);
---
+++ src/test/java/com/bff/nomina/nomina/config/TestConfig.java
@@ -0,0 +1,41 @@
+package com.bff.nomina.nomina.config;
+
+import com.bff.nomina.nomina.adapter.rest.handler.RestTemplateErrorHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@TestConfiguration
+public class TestConfig {
+
+    /**
+     * SCOPE_PROTOTYPE is used so that each adapter instance can customize the error handling.
+     *
+     * @param restTemplateBuilder Builder
+     * @return RestTemplate
+     */
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public RestTemplate getRestTemplate(
+            final RestTemplateBuilder restTemplateBuilder,
+            @Value("${rest.client.connect-timeout}") final int connectTimeout,
+            @Value("${rest.client.read-timeout}") final int readTimeout,
+            final ObjectMapper objectMapper
+    ) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(connectTimeout))
+                .setReadTimeout(Duration.ofMillis(readTimeout))
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .errorHandler(new RestTemplateErrorHandler(objectMapper))
+                .build();
+    }
+}
