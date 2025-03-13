0085ee107b8014398ede10e549430e020be2e890
Salvador <salvador.woini@gmail.com>
2025-03-12 23:47:17 -0300

    feat: minor fixes

--- src/main/java/com/bff/nomina/nomina/config/ErrorCode.java
+++ src/main/java/com/bff/nomina/nomina/config/ErrorCode.java
@@ -1,17 +1,18 @@
 package com.bff.nomina.nomina.config;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 
 public class ErrorCode implements Serializable {
 
     public static final ErrorCode INTERNAL_ERROR = new ErrorCode("999", "Internal server error");
     public static final ErrorCode BAD_REQUEST = new ErrorCode("101", "Bad request");
-    public static final ErrorCode RESOURCE_NOT_FOUND = new ErrorCode("102", "Not found");
     public static final ErrorCode NOMINA_NOT_FOUND = new ErrorCode("103", "Nomina not found");
-    public static final ErrorCode FORBIDDEN = new ErrorCode("104", "Not allowed to access the resource");
     public static final ErrorCode TIMEOUT = new ErrorCode("105", "Timeout");
 
     private final String value;
+    @Getter
     private final String reasonPhrase;
 
     public ErrorCode(final String value, final String reasonPhrase) {
@@ -21,10 +22,6 @@ public class ErrorCode implements Serializable {
 
     public String value() {
         return this.value;
-    }
-
-    public String getReasonPhrase() {
-        return this.reasonPhrase;
     }
 
 }
